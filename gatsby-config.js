2078ee2f00b86250fe7d95ab5a85a482b3380149
PopBang <popbang@mail.com>
2021-11-15 11:20:41 +0000

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,32 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `pop-bang-starter`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+
+        icon: `src/images/gatsby-icon.png`,
+
+        // This path is relative to the root of the site.
+      },
+    },
+  ],
 }
